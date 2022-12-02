1ed1beed7c6fb888d9c9a3d16703154373b0d2ce
kirtikhillari <119694214+kirtikhillari@users.noreply.github.com>
2022-12-02 23:04:37 +0530

    ExpliciteDemo.java

---
+++ ExplicitDemo.java
@@ -0,0 +1,11 @@
+public class ExplicitDemo{
+
+public static void main(String []args){
+
+double d1=38;
+double d2=10;
+System.out.println("value of double variable using floating literals:"+d1);
+System.out.println("Value of double variable usig floating literals:" +d2);
+}
+
+}
